13c4a8fd9f29f35ef9ca633ddcc6a20f27dc668a
박재중 <jjp910123@gmail.com>
2025-11-04 17:51:59 +0900

    test

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+const root = document.getElementById('root');
+
+if (root) {
+    const msg = document.createElement('h1');
+    msg.textContent = "Hello TypeScript + GitHub Pages! 🚀";
+    root.appendChild(msg);
+}
